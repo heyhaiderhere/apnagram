4c1beaf629d438e3789b7c8be6a046f5cd9337bb
Haider <raja@Haiders-MacBook-Pro.local>
2021-03-26 13:38:06 +0500

    database

--- connection.php
+++ connection.php
@@ -1,13 +1,12 @@
 <?php
 function serverQuery($query)
 {
-    $serverName = "localhost";
-    $userName = "root";
-    $password = "";
-    $selectDB = "instagram";
-    
+    $serverName = "remotemysql.com";
+    $userName = "NcLc7Ecx9a";
+    $password = "Pv1RkqGQCU";
+    $selectDB = "NcLc7Ecx9a";
+
     $connection = mysqli_connect($serverName, $userName, $password, $selectDB);
     $result = mysqli_query($connection, $query);
     return $result;
 }
-?>
